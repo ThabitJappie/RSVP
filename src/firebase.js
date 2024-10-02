f481c392a84dadf84f3888bbb7591dde9759f177
ThabitJappie <thabitj@yahoo.co.uk>
2024-10-02 21:11:51 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,20 @@
-// Import necessary Firebase modules
-import { initializeApp } from "firebase/app";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-// Your Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyCpHZowEZoQmZp9RZWfp--9W-hpRgAtOag",
+  apiKey: "AIzaSyCpHZowEZoQmZp9RZWfp--9W-hpRgAtOag",
   authDomain: "rsvp-62fb3.firebaseapp.com",
   projectId: "rsvp-62fb3",
   storageBucket: "rsvp-62fb3.appspot.com",
   messagingSenderId: "118533972066",
   appId: "1:118533972066:web:1526d3faf605cce2f6f13b",
-  measurementId: "G-MMTKVPQD61",
+  measurementId: "G-MMTKVPQD61"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // Export the auth instance
-export const db = getFirestore(app); // Export Firestore if needed
+const auth = getAuth(app);
 
-// Export RecaptchaVerifier and signInWithPhoneNumber for use in components
-export { RecaptchaVerifier, signInWithPhoneNumber };
+export { auth, RecaptchaVerifier, signInWithPhoneNumber };
+
