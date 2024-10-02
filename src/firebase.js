a72127b6860a3243eade9fd9a405b18e221ca08c
ThabitJappie <thabitj@yahoo.co.uk>
2024-10-02 21:19:53 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpHZowEZoQmZp9RZWfp--9W-hpRgAtOag",
+ apiKey: "AIzaSyCpHZowEZoQmZp9RZWfp--9W-hpRgAtOag",
   authDomain: "rsvp-62fb3.firebaseapp.com",
   projectId: "rsvp-62fb3",
   storageBucket: "rsvp-62fb3.appspot.com",
   messagingSenderId: "118533972066",
-  appId: "1:118533972066:web:1526d3faf605cce2f6f13b",
-  measurementId: "G-MMTKVPQD61"
+  appId: "1:118533972066:web:84a6c082d6fc096df6f13b",
+  measurementId: "G-7LTKKP1E35"
 };
 
 // Initialize Firebase
